2b0d085ab164ad1769f82af3a17dbfe602e737d9
Ang Jun Ray <angjunray@live.com>
2021-04-03 12:12:19 +0800

    Initial commit

---
+++ Lesson 4-Jokes App/Lesson 4-Jokes App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 4-Jokes App
+//
+//  Created by Ang Jun Ray on 3/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
