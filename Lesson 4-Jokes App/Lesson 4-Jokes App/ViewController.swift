42c8d2cdf228e27bd1d6b2dc309ec5a5b39a4373
Ang Jun Ray <angjunray@live.com>
2021-04-06 15:28:35 +0800

    PODs!

--- Lesson 4-Jokes App/Lesson 4-Jokes App/ViewController.swift
+++ Lesson 4-Jokes App/Lesson 4-Jokes App/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var test: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        doStuff()
     }
-
+    
+    func doStuff(){
+        let db = FirebaseFirestore.Firestore.firestore()
+        
+        db.collection("test").document("woo").setData(["HIII" : 1])
+    }
 
 }
 
